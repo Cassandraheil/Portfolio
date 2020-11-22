a720be149f72d9a0f76c4e615e75da0d55013e88
Cassandra Heil <Cassandraheil19@gmail.com>
2020-11-22 17:08:24 -0500

    styled to not look like other carousel

--- owlcarousel/carousel.js
+++ owlcarousel/carousel.js
@@ -3,16 +3,16 @@ var owl = $('.owl-carousel');
 owl.owlCarousel({
     items:4,
     loop:true,
-    margin:30,
+    margin:5,
     autoplay:true,
-    autoplayTimeout:500,
+    autoplayTimeout:2000,
     autoplayHoverPause:false,
-    smartSpeed :700,
-    nav: true,
-    navText: ["<i class='fas fa-angle-double-left'></i>", "<i class='fas fa-angle-double-right'></i>"]
+    smartSpeed :10000,
+    nav: true
+    // navText: ["<i class='fas fa-angle-double-left'></i>", "<i class='fas fa-angle-double-right'></i>"]
 });
 $('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
+    owl.trigger('play.owl.autoplay',[2000])
 })
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
